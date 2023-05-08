95c7ca67e5b9cc9b418058ff9221bfd684737802
B0rjomi <valerkalifanov@gmail.com>
2023-05-08 22:26:36 +0300

    fix gitignore

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from crop_matrix import *
 if __name__ == '__main__':
     # show_multiple('pages/raw/8.jpg', 'rects/labels/8.json')
     # multiple_crop('pages/raw/8.jpg', 'rects/labels/8.json')
-    show_hist('pages/raw/1.jpg', 'rects/labels/1.json', 1)
+    # show_hist('pages/raw/1.jpg', 'rects/labels/1.json', 1)
+    # erode_thresh('crop_matrices/pic_1_matrix_0.jpg')
+    erode_for_all('crop_matrices')
